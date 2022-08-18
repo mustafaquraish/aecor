f20f5799abb4dd177550b9a86e6092b7c354de7f
Mustafa Quraish <mustafaq9@gmail.com>
2022-08-18 05:42:44 -0400

    Move `format_string` back to `vsnprintf`
    Apparently `vasprintf` is not super-well supported, so lets just
    do it manually. This might be a small performance hit but it doesn't
    really matter for now anyway

--- lib/prelude.h
+++ lib/prelude.h
@@ -1,13 +1,13 @@
 #include "stdarg.h"
-#include "stddef.h"
-#include "stdio.h"
-#include "stdlib.h"
-
 char* format_string(const char* format, ...) {
   va_list args;
   va_start(args, format);
-  char *formatted;
-  vasprintf(&formatted, format, args);
+  int size = vsnprintf(NULL, 0, format, args);
   va_end(args);
-  return formatted;
+  va_start(args, format);
+  char* s = calloc(1, size + 1);
+  vsprintf(s, format, args);
+  s[size] = '\0';
+  va_end(args);
+  return s;
 }
