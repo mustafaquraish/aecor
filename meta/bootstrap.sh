52bf65c31cd2cc3a3e3fc37bd2e6fac7210377ca
Mustafa Quraish <mustafaq9@gmail.com>
2022-08-18 05:51:16 -0400

    Have {gen,}bootstrap.sh create debug executables
    This is handy if something is broken and we want to figure out why

--- meta/bootstrap.sh
+++ meta/bootstrap.sh
@@ -11,11 +11,12 @@ echo "[+] Building stage 2 compiler"
 echo "[+] Building stage 3 compiler"
 ./build/stage2 compiler/main.ae -o build/stage3
 echo "[+] Comparing stage 2 and 3"
-# diff build/stage2.c build/stage3.c
+
 if diff build/stage2.c build/stage3.c; then
+    ./build/stage3 -s -d compiler/main.ae -o ./build/aecor
+    mv build/aecor bootstrap/aecor
     echo "[+] Bootstrap successful: Use ./bootstrap/aecor"
 else
     echo "[-] Error: Stage 2 and 3 are different"
     exit 1
 fi
-mv build/stage3 bootstrap/aecor
--- meta/gen_bootstrap.sh
+++ meta/gen_bootstrap.sh
@@ -35,8 +35,9 @@ read -p "Are you sure you want to replace bootstrap/stage0.c? [y/N] " confirm
 if [[ $confirm =~ ^[Yy]$ ]]; then
     echo "[+] cp build/stage3.c bootstrap/stage0.c"
     cp build/stage3.c bootstrap/stage0.c
-    echo "[+] cp build/stage3 bootstrap/aecor"
-    cp build/stage3 bootstrap/aecor
+    echo "[+] Creating debug version into /bootstrap/aecor"
+    ./build/stage3 -s -d compiler/main.ae -o ./build/aecor
+    cp build/aecor bootstrap/aecor
     echo "Done."
 else
     echo "[-] Aborting"
